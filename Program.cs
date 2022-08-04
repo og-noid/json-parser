fe1ef064f79f5d40a3457201d7d9ca15af19a5f2
Adam Norris <adamjnorris@protonmail.com>
2022-08-03 23:51:25 -0400

    Formatting

--- Program.cs
+++ Program.cs
@@ -9,16 +9,13 @@ using System.Threading.Tasks;
 
 namespace com.rivasolutions {
     class JsonParser {
-
         public static async Task Main(string[] args) {
-
             Task<JsonDocument> _task = getJson();
             JsonDocument _jsonDocument = await _task;
             Console.WriteLine(JsonDocumentToString(_jsonDocument));
         }
 
         public static async Task<JsonDocument> getJson() {
-
             string _apiKey = "nokey";
             string _keyFile = Path.Combine("apikey.txt");
             string _content = "{\"empty\":true}";
@@ -27,22 +24,18 @@ namespace com.rivasolutions {
                 _apiKey = File.ReadAllText(_keyFile);
                 using var _client = new HttpClient();
                 var _result = await _client.GetAsync(
-                    string.Format("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={0}", 
+                    string.Format("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={0}",
                     _apiKey)
                 );
                 _content = await _result.Content.ReadAsStringAsync();
-                // Console.WriteLine(_content);
-
             } else {
                 Console.WriteLine("File {0} does not exist, cannot execute API call", _keyFile);
             }
             return JsonDocument.Parse(_content);
-
         }
 
         public static string JsonDocumentToString(JsonDocument jsonDocument) {
-            using(var stream = new MemoryStream())
-            {
+            using (var stream = new MemoryStream()) {
                 Utf8JsonWriter writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true });
                 jsonDocument.WriteTo(writer);
                 writer.Flush();
@@ -50,5 +43,5 @@ namespace com.rivasolutions {
                 return (_json);
             }
         }
-    }  
+    }
 }
